9f7c10266f5b59df1a14dfa5a5399032147ae95d
Sisa Mokranova <sisa.mokranova@gmail.com>
2023-12-03 06:44:33 +0100

    day 3 part 1

---
+++ day3-2.js
@@ -0,0 +1,70 @@
+let fileHandler = require('./fileHandler.js')
+let input = fileHandler.getInputArr('input3.txt')
+// 467835
+
+// 467..114..
+// ...*......
+// ..35..633.
+// ......#...
+// 617*......
+// .....+.58.
+// ..592.....
+// ......755.
+// ...$.*....
+// .664.598..
+
+let checkAround = function(i,j){
+    let lineLen = input[i].length
+    let endLine = input.length
+    for(ii = i-1; ii <= i+1; ii++){
+        for(jj = j-1; jj <= j+1; jj++){
+            if(jj > 0 && jj < endLine && ii > 0 && ii < lineLen){
+                if(isSpecial(input[ii][jj])){
+                    return true
+                }
+            }
+        }
+    }
+}
+
+let isSpecial = function(it){
+    return !isNumber(it) && it !== "."
+}
+
+let isNumber = function(it){
+    return parseInt(it).toString() == it
+}
+
+let i = 0;
+let sum = 0;
+let num = "";
+let add = false
+let arr = []
+for(line of input){
+    let arrIn = []
+    for(char of line){
+        arrIn.push(0)
+    }
+    arr.push(arrIn)
+}
+
+for(line of input){
+    let j = 0;
+    for(char of line){
+        if(isNumber(char)){
+            num += char
+            if (checkAround(i, j)){
+                add = true
+            }
+        } else {
+            arr[i][j] = isNumber(num) && add ? parseInt(num) : 0;
+            sum += isNumber(num) && add ? parseInt(num) : 0;
+           // console.log(isNumber(num) && add ? num : "")
+            num = ""
+            add = false
+        }
+        j++
+    }
+    i++
+}
+console.log(arr)
