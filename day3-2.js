6baa089d9075cd738eece113b78ef61fa9e843ad
Sisa Mokranova <sisa.mokranova@gmail.com>
2023-12-03 07:24:44 +0100

    absolute garbage fire day 3 part 2

--- day3-2.js
+++ day3-2.js
@@ -13,6 +13,15 @@ let input = fileHandler.getInputArr('input3.txt')
 // ...$.*....
 // .664.598..
 
+let i = 0;
+let sum = 0;
+let num = "";
+let add = false
+let arr = []
+let iLen = input[0].length
+let jLen = input.length
+console.log(iLen, jLen)
+
 let checkAround = function(i,j){
     let lineLen = input[i].length
     let endLine = input.length
@@ -35,14 +44,9 @@ let isNumber = function(it){
     return parseInt(it).toString() == it
 }
 
-let i = 0;
-let sum = 0;
-let num = "";
-let add = false
-let arr = []
-for(line of input){
+for(let i = 0; i < iLen; i++){
     let arrIn = []
-    for(char of line){
+    for(let j = 0; j < jLen; j++){
         arrIn.push(0)
     }
     arr.push(arrIn)
@@ -57,14 +61,66 @@ for(line of input){
                 add = true
             }
         } else {
-            arr[i][j] = isNumber(num) && add ? parseInt(num) : 0;
-            sum += isNumber(num) && add ? parseInt(num) : 0;
-           // console.log(isNumber(num) && add ? num : "")
+            let len = num.length
+            for(y = j - 1; y >= j - len; y --){
+                arr[i][y] = isNumber(num) ? parseInt(num) : ".";
+            }
             num = ""
             add = false
+        }
+        if(arr[i][j] === 0 && !isNumber(char)){
+            arr[i][j] = char
         }
         j++
     }
     i++
 }
-console.log(arr)
+
+let checkAround2 = function(i,j){
+    let lineLen = arr[i].length
+    let endLine = arr.length
+    let sum = 0
+    let nums = []
+    for(ii = i-1; ii <= i+1; ii++){
+        for(jj = j-1; jj <= j+1; jj++){
+            if(jj >= 0 && jj < endLine && ii >= 0 && ii < lineLen){
+                if(isSpecial2(arr[ii][jj])){
+                    if(!nums.includes(arr[ii][jj])){ nums.push(arr[ii][jj])}
+                }
+            }
+        }
+    }
+    if(nums.length == 2) {
+        sum = nums[0] * nums[1]
+    }
+    console.log(nums)
+    return sum
+}
+let isSpecial2 = function(it){
+    return isNumber2(it)
+}
+
+let isNumber2 = function(it){
+    return typeof it === 'number'
+}
+
+
+i = 0
+
+for(line of arr){
+    j = 0
+    for(char of line){
+        if(char === "*"){
+            sum += checkAround2(i, j)
+        }
+        j++
+    }
+    i++
+}
+
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr[4])
+console.log(sum)
